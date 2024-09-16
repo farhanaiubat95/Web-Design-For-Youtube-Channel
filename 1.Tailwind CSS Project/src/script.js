3492abddef757da7293a04ec78a56a043cd88a7a
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-17 03:04:11 +0600

    update js file

---
+++ 1.Tailwind CSS Project/src/script.js
@@ -0,0 +1,41 @@
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".slide");
+const bg = document.querySelectorAll(".blue-banner-bg");
+
+var sliderNav = function (manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    bg.forEach((bgCol) => {
+        bgCol.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    bg[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i)
+    });
+});
+
+/*=============================================
+    Nav background color change when scrolling
+===============================================*/
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 100) {
+        document.getElementById("nav-gold").style.display = "block";
+    } else if (window.scrollY < 100) {
+        document.getElementById("nav-gold").style.display = "none";
+    }
+});
+
+AOS.init();
